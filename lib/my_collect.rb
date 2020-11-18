410b12422a970f87975336fc173669bc790bff0e
Nausheen Akhter <nausheen701@gmail.com>
2020-11-18 17:55:33 +0300

    commit tree 4b1c3b2402d8855bc221408eb0fbc5043788884d

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
 
+def my_collect(objects)
+    i = 0
+    new_array = []
+    while i < objects.length
+        new_array << yield(objects[i])
+        i += 1
+    end
+    new_array
+
+    #objects.collect {|action| action.upcase}   
+    
+    #objects.collect {|action1| action1.split(" ").first}
+    
+end
